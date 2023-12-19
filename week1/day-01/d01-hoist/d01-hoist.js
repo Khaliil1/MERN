e7eb7ceda383c7d9173f2738ea0ef7872dc1c169
Khaliil1 <khalilmeddeb123@gmail.com>
2023-12-19 17:38:14 +0100

    commit

--- week1/day-01/d01-hoist/d01-hoist.js
+++ week1/day-01/d01-hoist/d01-hoist.js
@@ -1,77 +1,90 @@
+var hello;
 console.log(hello);
-var hello = 'world';
+hello = 'world';
 // undefined
 // ******************************************
 
-var needle = 'haystack';
-// undefined
-test();
+var needle;
 function test() {
-    var needle = 'magnet';
+    var needle;
+    needle = 'magnet';
     console.log(needle);
 }
+
+needle = 'haystack';
+
+test();
 // magnet
 // *****************************************
 
-var brendan = 'super cool';
+var brendan;
 function print() {
     brendan = 'only okay';
     console.log(brendan);
 }
+
+brendan = 'super cool';
 console.log(brendan);
 
 // supercool
 // ****************************************
 
-var food = 'chicken';
-console.log(food);
-// chicken
-eat();
+var food;
 function eat() {
+    var food;
     food = 'half-chicken';
     console.log(food);
-    var food = 'gone';
+    food = 'gone';
 }
+
+food = 'chicken';
+console.log(food);
+eat();
+
+
 // half-Chicken
 // ****************************************
-
-mean();
+var mean;
+// mean();
 console.log(food);
-var mean = function () {
+mean = function () {
+    var food;
     food = "chicken";
     console.log(food);
-    var food = "fish";
+    food = "fish";
     console.log(food);
 }
 console.log(food);
-
-// no function
-
+//chicken
+// fish
 // *****************************************
-console.log(genre);
-// undefined
-var genre = "disco";
-rewind();
+var genre;
 function rewind() {
+    var genre;
     genre = "rock";
     console.log(genre);
-    var genre = "r&b";
+    genre = "r&b";
     console.log(genre);
 }
+
+console.log(genre);
+genre = "disco";
+rewind();
 console.log(genre);
 // rock
 // r&b
 // disco
 // ****************************************
 
+var dojo;
 dojo = "san jose";
 console.log(dojo);
-// san jose
 learn();
 function learn() {
+    var dojo;
     dojo = "seattle";
     console.log(dojo);
-    var dojo = "burbank";
+    dojo = "burbank";
     console.log(dojo);
 }
 console.log(dojo);
@@ -80,19 +93,20 @@ console.log(dojo);
 // san jose
 
 // *****************************************
-
 console.log(makeDojo("Chicago", 65));
 console.log(makeDojo("Berkeley", 0));
+
 function makeDojo(name, students) {
     const dojo = {};
     dojo.name = name;
     dojo.students = students;
+
     if (dojo.students > 50) {
         dojo.hiring = true;
+    } else if (dojo.students <= 0) {
+        dojo.status = "closed for now";
     }
-    else if (dojo.students <= 0) {
-        dojo = "closed for now";
-    }
+
     return dojo;
 }
 
